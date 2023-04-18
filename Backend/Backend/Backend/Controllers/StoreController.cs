37eb8adf55e8a99bd20ccf8364b66409292306d7
amitgrum8 <63203500+amitgrum8@users.noreply.github.com>
2023-04-18 12:43:46 +0300

    fixing stupid bug

---
+++ Backend/Backend/Backend/Controllers/StoreController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using Backend.Business.src.Utils;
+using Backend.Controllers.Requests;
+using Backend.Service;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreController : ControllerBase
+    {
+
+        public StoreController()
+        {
+            //127.128.0.0 ()
+        }
+
+        public Response<bool> Login([FromBody] storeLoginRequest request)
+        {
+            return Store.Instance.Login(request.accountNumber, request.storeId, request.password);
+        }
+        
+        //int StoreId string from string too
+        [HttpPost("SeeReports")]
+        public bool SeeReports([FromBody] PurchaseHistoryRequest request)
+        {
+            Console.WriteLine("yess!!!");
+            //Console.WriteLine(request.email);
+            return true;
+        }
+
+        //int StoreId 
+        [HttpPost("sendbyemail")]
+        public bool SendByEmail([FromBody] UserDataRequest request)
+        {
+
+            Console.WriteLine("yess!!!");
+            Console.WriteLine(request.email);
+            return true;
+        }
+
+        //int StoreId 
+        [HttpPost("sendbysms")]
+        public bool SendBySms([FromBody] UserDataRequest request)
+        {
+
+            Console.WriteLine("yess!!!");
+            Console.WriteLine(request.email);
+            return true;
+        }
+      
+
+        [HttpPost("openChatStore")]
+        //int userId
+        public Response<int> openChat([FromBody] openChatRequest request)
+        {
+            return Store.Instance.OpenChat(request.StoreId, request.userId);
+        }
+        [HttpPost("sendMassageInChat")]
+        //json.stringfy(message)
+        public Response<string> sendMassageInChat([FromBody] chatMassageRequest request)
+        {
+            return Store.Instance.SendMessage(request.StoreId, request.SessionId, request.Text);
+        }
+        //list(json.stringfy(chats)
+        [HttpPost("getAllchats")]
+        public Response<List<String>> getAllchats(int StoreId)
+        {
+            return Store.Instance.GetAllchats(StoreId);
+        }
+
+        // [HttpPost("closeChatStore")]
+        // public Response<int> closeChat([FromBody] openChatRequest request)
+        // {
+            
+        // }
+
+
+
+    }
+}
